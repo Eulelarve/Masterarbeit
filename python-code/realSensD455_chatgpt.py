5f9a3e06739ef359bbdacc4b64c2b4a640c24bcb
jonas wegener <sanoj14@live.de>
2026-06-09 20:09:35 +0200

    1 fotos mehr

--- python-code/realSensD455_chatgpt.py
+++ python-code/realSensD455_chatgpt.py
@@ -9,7 +9,7 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # RGB Stream
-config.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Tiefen-Stream
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
