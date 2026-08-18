baef927acdb0cd2b10e2090a3056f6303ec002dd
Eulelarve <sanoj14@live.de>
2026-08-18 20:07:25 +0200

    home...  methoden übernahme

--- python-code/test.py
+++ python-code/test.py
@@ -2,7 +2,7 @@ import numpy as np
 from Vision_Robotic_Arm_Gesture_Recognition.own_functions import ValueBuffer, close_to, median
 from collections import deque
 
-t = [1,2,3,4,4,5][:]
-buffer = deque(maxlen=4)
-# buffer.append(2)
-print(np.round(4.564,None,))
+import math
+
+t= math.atan2(0,0)
+print(math.degrees(t))
